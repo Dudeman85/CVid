9acbb57b3a341f62b2a73b92186e31cb3df18d92
Aleksi Anderson <anderson.aleksi@gmail.com>
2025-03-21 17:58:36 +0200

    Finished demo

--- sandboxes/sandbox2/main.cpp
+++ sandboxes/sandbox2/main.cpp
@@ -1,5 +1,4 @@
 #include <format>
-#include <thread>
 #include <chrono>
 #include <filesystem>
 #include <cvid/Window.h>
@@ -25,18 +24,34 @@ int main()
 	const float minZoom = 0;
 	const float maxZoom = 700;
 	//Transition parameters
-	const float minSpawn = 100;
-	const float maxSpawn = 1300;
+	const float minSpawn = 250;
+	const float maxSpawn = 1500;
 	const float minSpeed = 1000;
 	const float maxSpeed = 4000;
 
+	//Cursor data
+	POINT currentCursorPos;
+	POINT lastCursorPos;
+	HWND hWnd = GetConsoleWindow();
+	bool clickedInWindow = false;
+
+	//Timers
+	double deltaTime = 0;
+	double avgDt = 0;
+	double timeSinceLastAvg = 0;
+	std::vector<double> previousDts;
+	const float transitionDelay = 0.5;
+	double transitionTimer = 0;
+	double transitionDir = 0;
+
+
 	//Make window
 	cvid::Vector2Int windowSize = { 160, 90 };
-	cvid::Window window(windowSize.x, windowSize.y, "CVid", false);
+	cvid::Window window(windowSize.x, windowSize.y, "CVid Demo", false);
 	window.enableDepthTest = true;
 
 	//Make camera
-	cvid::Camera cam(cvid::Vector3(0, 0, 100), windowSize.x, windowSize.y);
+	cvid::Camera cam(cvid::Vector3(0, -10, 150), windowSize.x, windowSize.y);
 	float fov = 90;
 	cam.MakePerspective(fov, 1, 5000);
 	cam.Rotate(cvid::Vector3(0, cvid::Radians(0), 0));
@@ -57,16 +72,22 @@ int main()
 	displayModel.SetScale(20);
 	displayModel.SetPosition({ 0, 0, 0 });
 	//Use a quaternion here to store cube rotation since it's not properly implemented in engine yet
-	quaternion::Quaternion<double> modelRotationQuat = quaternion::from_euler(std::array{ 0, 0, 0 });
+	quaternion::Quaternion<double> modelRotationQuat(0.7150, -0.4207, -0.3940, -0.0848);
+	cvid::Matrix4 rotation = cvid::Matrix4::Identity();
+	//Convert from quaternion to cvid matrix
+	auto mat = quaternion::to_rotation_matrix(modelRotationQuat);
+	for (size_t i = 0; i < mat.size(); i++)
+	{
+		for (size_t j = 0; j < mat[0].size(); j++)
+		{
+			rotation[i][j] = mat[i][j];
+		}
+	}
 
-
-	double deltaTime = 0;
-	POINT currentCursorPos;
-	POINT lastCursorPos;
-	bool lcDown = false;
-	const float transitionDelay = 1;
-	float transitionTimer = 0;
-	double transitionDir = 0;
+	//Make the second model in swapping
+	cvid::ModelInstance swapModel(&models[1]);
+	swapModel.SetScale(20);
+	swapModel.SetPosition({ 0, 0, 0 });
 
 	while (true)
 	{
@@ -75,53 +96,51 @@ int main()
 		if (GetKeyState(VK_ESCAPE) & 0x8000)
 			return 0;
 
-		//When the model transition animation is going dont respond to input
-		if (transitionTimer <= 0)
+		//Update cursor delta
+		if (GetKeyState(VK_LBUTTON) & 0x8000 && clickedInWindow)
 		{
-			//Update cursor delta
-			if (GetKeyState(VK_LBUTTON) & 0x8000)
+			lastCursorPos = currentCursorPos;
+			GetCursorPos(&currentCursorPos);
+
+			double dx = currentCursorPos.x - lastCursorPos.x;
+			double dy = currentCursorPos.y - lastCursorPos.y;
+
+			//Rotate model by mouse delta
+			if (dx != 0 || dy != 0)
 			{
-				lastCursorPos = currentCursorPos;
-				GetCursorPos(&currentCursorPos);
+				//Rotate using quaternions
+				dx *= -rotationSpeed / 2;
+				dy *= -rotationSpeed / 2;
+				modelRotationQuat *= quaternion::Quaternion(cos(dx), 0.0, sin(dx), 0.0);
+				modelRotationQuat *= quaternion::Quaternion(cos(dy), sin(dy), 0.0, 0.0);
 
-				double dx = currentCursorPos.x - lastCursorPos.x;
-				double dy = currentCursorPos.y - lastCursorPos.y;
-
-				//Rotate model by mouse delta
-				if (dx != 0 || dy != 0)
+				//Convert from quaternion to cvid matrix
+				auto mat = quaternion::to_rotation_matrix(modelRotationQuat);
+				rotation = cvid::Matrix4::Identity();
+				for (size_t i = 0; i < mat.size(); i++)
 				{
-					//Manually create the transform matrix using a quaternion rotation
-					cvid::Matrix4 transform = cvid::Matrix4::Identity();
-					transform = transform.Scale(displayModel.GetScale());
-
-					//Rotate using quaternions
-					dx *= -rotationSpeed / 2;
-					dy *= -rotationSpeed / 2;
-					modelRotationQuat *= quaternion::Quaternion(cos(dx), 0.0, sin(dx), 0.0);
-					modelRotationQuat *= quaternion::Quaternion(cos(dy), sin(dy), 0.0, 0.0);
-
-					//Convert from quaternion to cvid matrix
-					auto mat = quaternion::to_rotation_matrix(modelRotationQuat);
-					cvid::Matrix4 rotation = cvid::Matrix4::Identity();
-					for (size_t i = 0; i < mat.size(); i++)
+					for (size_t j = 0; j < mat[0].size(); j++)
 					{
-						for (size_t j = 0; j < mat[0].size(); j++)
-						{
-							rotation[i][j] = mat[i][j];
-						}
+						rotation[i][j] = mat[i][j];
 					}
-					transform = transform * rotation;
-
-					transform = transform.Translate(displayModel.GetPosition());
-
-					displayModel.SetTransform(transform);
 				}
 			}
-			else
-			{
-				GetCursorPos(&currentCursorPos);
-			}
+		}
+		else
+		{
+			GetCursorPos(&currentCursorPos);
+			//Make sure the user clicked insisde the window
+			RECT bounds;
+			GetClientRect(hWnd, &bounds);
+			ClientToScreen(hWnd, (LPPOINT)&bounds);
+			ClientToScreen(hWnd, (LPPOINT)&bounds + 1);
+			clickedInWindow = currentCursorPos.x > bounds.left && currentCursorPos.x < bounds.right
+				&& currentCursorPos.y > bounds.top && currentCursorPos.y < bounds.bottom;
+		}
 
+		//When the model transition animation is going dont respond to zoom or arrows
+		if (transitionTimer <= 0)
+		{
 			//Get the console input record for scroll wheel
 			//Does not work in seperate window
 			std::vector<INPUT_RECORD> ir = window.GetInputRecord();
@@ -155,55 +174,91 @@ int main()
 			//Decrement the display model
 			if (GetKeyState(VK_LEFT) & 0x8000)
 			{
+				swapModel.SetBaseModel(&models[currentModel]);
+				swapModel.SetPosition({ 0, 0, 0 });
+
 				currentModel--;
 				if (currentModel < 0)
 					currentModel = models.size() - 1;
-
-				displayModel.SetBaseModel(&models[currentModel]);
-				displayModel.SetPosition({ std::lerp(minSpawn, maxSpawn, cam.GetPosition().z / maxZoom), 0, 0});
-				transitionTimer = transitionDelay;
-				transitionDir = -std::lerp(minSpeed, maxSpeed, cam.GetPosition().z / maxZoom);
-			}
-			//Increment the display model
-			if (GetKeyState(VK_RIGHT) & 0x8000)
-			{
-				currentModel++;
-				if (currentModel > models.size() - 1)
-					currentModel = 0;
 
 				displayModel.SetBaseModel(&models[currentModel]);
 				displayModel.SetPosition({ -std::lerp(minSpawn, maxSpawn, cam.GetPosition().z / maxZoom), 0, 0 });
 				transitionTimer = transitionDelay;
 				transitionDir = std::lerp(minSpeed, maxSpeed, cam.GetPosition().z / maxZoom);
 			}
+			//Increment the display model
+			if (GetKeyState(VK_RIGHT) & 0x8000)
+			{
+				swapModel.SetBaseModel(&models[currentModel]);
+				swapModel.SetPosition({ 0, 0, 0 });
+
+				currentModel++;
+				if (currentModel > models.size() - 1)
+					currentModel = 0;
+
+				displayModel.SetBaseModel(&models[currentModel]);
+				displayModel.SetPosition({ std::lerp(minSpawn, maxSpawn, cam.GetPosition().z / maxZoom), 0, 0 });
+				transitionTimer = transitionDelay;
+				transitionDir = -std::lerp(minSpeed, maxSpeed, cam.GetPosition().z / maxZoom);
+			}
 		}
 		else
 		{
-			GetCursorPos(&currentCursorPos);
-
 			transitionTimer -= deltaTime;
 
 			//Move the new model in from the side
 			if (abs(displayModel.GetPosition().x) > abs(transitionDir) * deltaTime)
 				displayModel.Translate({ transitionDir * deltaTime, 0, 0 });
 			else
-				displayModel.SetPosition({0, 0, 0});
+				displayModel.SetPosition({ 0, 0, 0 });
+
+			//Move the old model to the side
+			swapModel.Translate({ transitionDir * deltaTime, 0, 0 });
 		}
+
 
 		//Start rendering
 		window.Fill({ 0, 0, 0 });
 		window.ClearDepthBuffer();
 
+		//Manually create the transform matrix using a quaternion rotation
+		cvid::Matrix4 transform = cvid::Matrix4::Identity();
+		transform = transform.Scale(displayModel.GetScale());
+		transform = transform * rotation;
+		transform = transform.Translate(displayModel.GetPosition());
+		displayModel.SetTransform(transform);
+
 		cvid::DrawModel(&displayModel, &cam, &window);
+
+		//Draw the swap model if transitioning
+		if (transitionTimer > 0)
+		{
+			cvid::Matrix4 transform = cvid::Matrix4::Identity();
+			transform = transform.Scale(swapModel.GetScale());
+			transform = transform * rotation;
+			transform = transform.Translate(swapModel.GetPosition());
+			swapModel.SetTransform(transform);
+			cvid::DrawModel(&swapModel, &cam, &window);
+		}
 
 		//Display the model name text
 		std::string name = std::format("<- {} ->", displayModel.GetBaseModel()->name);
 		cvid::Vector2Int namePos = { windowSize.x / 2 - (int)name.size() / 2, windowSize.y / 2 - 2 };
 		window.PutString(namePos, name);
 
-		//Display the fps
-		std::string fps = std::format("{} fps", std::floor(1 / deltaTime));
-		cvid::Vector2Int fpsPos = { windowSize.x - (int)fps.size() - 1, 1 };
+		//Update the average fps 5 times a second
+		if (timeSinceLastAvg > 0.2)
+		{
+			avgDt = 0;
+			for (double dt : previousDts)
+				avgDt += dt;
+			avgDt /= previousDts.size();
+			previousDts.clear();
+			timeSinceLastAvg = 0;
+		}
+		timeSinceLastAvg += deltaTime;
+		std::string fps = std::format("{} fps", std::floor(1 / avgDt));
+		cvid::Vector2Int fpsPos = { windowSize.x - (int)fps.size() - 2, 1 };
 		window.PutString(fpsPos, fps);
 
 		if (!window.DrawFrame())
@@ -213,6 +268,7 @@ int main()
 		window.SendData("\x1b[0;0H", 7, cvid::DataType::String);
 
 		deltaTime = cvid::EndTimePoint();
+		previousDts.push_back(deltaTime);
 	}
 
 	return 0;
